ab1456dae0b2420604be321cd9a4f5aac3fb6093
Rafael Mota <fael.folow@gmail.com>
2022-07-28 18:49:48 -0300

    Adiciona o funcionamento dos contadores
    Cria o código JavaScript responsável pela contagem regressiva.

---
+++ script.js
@@ -0,0 +1,32 @@
+const diasElemento = document.getElementById("dias");
+const horasElemento = document.getElementById("horas");
+const minutosElemento = document.getElementById("minutos");
+const segundosElemento = document.getElementById("segundos");
+
+const inicioCenso = "1 Aug 2022"
+
+function contador(){
+    const censoData = new Date(inicioCenso);
+    console.log(censoData)
+    const dataAtual = new Date();
+
+    const totalSegundos = (censoData - dataAtual) / 1000;
+    
+    const dias = Math.floor(totalSegundos / 3600 / 24);
+    const horas = Math.floor(totalSegundos / 3600) % 24;
+    const minutos = Math.floor(totalSegundos / 3600) % 60;
+    const segundos = Math.floor(totalSegundos) % 60;
+
+    diasElemento.innerHTML = dias;
+    horasElemento.innerHTML = formatTime(horas);
+    minutosElemento.innerHTML = formatTime(minutos);
+    segundosElemento.innerHTML = formatTime(segundos);
+}
+
+function formatTime(time){
+    return time < 10 ? '0' + time : time;
+}
+
+contador();
+
+setInterval(contador, 1000);
