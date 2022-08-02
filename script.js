a7e22611247d50fa78077fb57068238bf460c13a
Rafael Mota <fael.folow@gmail.com>
2022-08-01 22:11:10 -0300

    Adiciona condicao para o termino do contador
    Cria uma condicao if-else no arquivo JavaScript para quando a contagem
    regressiva acabar, deixando o contador zerado e exibindo uma mensagem
    diferente no descritivo.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const diasElemento = document.getElementById("dias");
 const horasElemento = document.getElementById("horas");
 const minutosElemento = document.getElementById("minutos");
 const segundosElemento = document.getElementById("segundos");
+const descritivo = document.getElementById("descritivo-imagem")
 
 const inicioCenso = "1 Aug 2022"
 
@@ -11,16 +12,24 @@ function contador(){
     const dataAtual = new Date();
 
     const totalSegundos = (censoData - dataAtual) / 1000;
-    
+
     const dias = Math.floor(totalSegundos / 3600 / 24);
     const horas = Math.floor(totalSegundos / 3600) % 24;
     const minutos = Math.floor(totalSegundos / 60) % 60;
     const segundos = Math.floor(totalSegundos) % 60;
 
-    diasElemento.innerHTML = formatTime(dias);
-    horasElemento.innerHTML = formatTime(horas);
-    minutosElemento.innerHTML = formatTime(minutos);
-    segundosElemento.innerHTML = formatTime(segundos);
+    if (totalSegundos > 0){
+        diasElemento.innerHTML = formatTime(dias);
+        horasElemento.innerHTML = formatTime(horas);
+        minutosElemento.innerHTML = formatTime(minutos);
+        segundosElemento.innerHTML = formatTime(segundos);
+    } else if (totalSegundos < 0){
+        diasElemento.innerHTML = '00';
+        horasElemento.innerHTML = '00';
+        minutosElemento.innerHTML = '00';
+        segundosElemento.innerHTML = '00';
+        descritivo.innerHTML = 'O censo do IBGE já começou!!!'
+    }    
 }
 
 function formatTime(time){
